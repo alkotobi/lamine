3a18b338589b101d0b1ef241d7f5f6513088dfd1
merhab <merhab@gmail.com>
2025-03-27 09:51:30 +0800

    staert calc fields

--- udtm_main.pas
+++ udtm_main.pas
@@ -12,14 +12,26 @@ type
   { Tdtm }
 
   Tdtm = class(TDataModule)
+    car_name: TWideStringField;
     city_name: TWideStringField;
+    client_name: TWideStringField;
     country_name: TWideStringField;
+    dts_order: TDataSource;
     dts_groups: TDataSource;
     dts_car_name: TDataSource;
     dts_brand: TDataSource;
     group_name: TWideStringField;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
+    order_details_client_name: TWideStringField;
+    qry_car_modelsid: TLargeintField;
+    qry_car_modelsid_car_name: TLongintField;
+    qry_car_modelsmodel: TWideStringField;
+    qry_car_modelsnotes: TWideMemoField;
+    qry_car_namesbrand_name: TStringField;
+    qry_car_namesid: TLargeintField;
+    qry_car_namesid_brand: TLongintField;
+    qry_car_namesname: TWideStringField;
     qry_citiesid: TLargeintField;
     qry_citiesname: TWideStringField;
     qry_citiespostal_code: TWideStringField;
@@ -34,6 +46,27 @@ type
     qry_clientsstreet: TWideStringField;
     qry_count_zero: TUniQuery;
     qry_count_zerocount_zero_users: TLargeintField;
+    qry_orderdate: TDateField;
+    qry_orderid: TLargeintField;
+    qry_orderid_client: TLongintField;
+    qry_orderref: TWideStringField;
+    qry_ordervirsement: TFloatField;
+    qry_order_details: TUniQuery;
+    qry_order_detailscolor: TLongintField;
+    qry_order_detailsdate_of_delivery: TDateField;
+    qry_order_detailsdate_of_production: TDateField;
+    qry_order_detailsdate_ordered: TDateField;
+    qry_order_detailsfrieght_usd: TFloatField;
+    qry_order_detailsid: TLargeintField;
+    qry_order_detailsid_car_model: TLongintField;
+    qry_order_detailsid_client: TLongintField;
+    qry_order_detailsid_order: TLongintField;
+    qry_order_detailskm: TLongintField;
+    qry_order_detailsprice_usd: TFloatField;
+    qry_order_detailsrate: TFloatField;
+    qry_order_detailsship_from_id_city: TLongintField;
+    qry_order_detailsship_to_id_city: TLongintField;
+    qry_order_detailsvin: TWideStringField;
     qry_transactionsamount: TFloatField;
     qry_transactionsamount_received: TFloatField;
     qry_transactionsamount_usd: TCurrencyField;
@@ -64,6 +97,8 @@ type
     qry_clients: TUniQuery;
     qry_cities: TUniQuery;
     qry_countries: TUniQuery;
+    qry_order: TUniQuery;
+    procedure qry_car_namesCalcFields(DataSet: TDataSet);
     procedure qry_transactionsCalcFields(DataSet: TDataSet);
   private
 
@@ -101,6 +136,12 @@ begin
   begin
     qry_transactionsamount_usd.AsCurrency:= qry_transactionsamount.AsCurrency/qry_transactionsrate.AsCurrency;
   end;
+end;
+
+procedure Tdtm.qry_car_namesCalcFields(DataSet: TDataSet);
+begin
+  qry_car_namesbrand_name.AsString:= qry_car_names.FieldByName('brand').AsString
+  +' '+qry_car_namesname.AsString;
 end;
 
 end.
