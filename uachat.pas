51b294ab76b9017cefeb49d37a15d9b6cba4e25d
merhab <merhab@merhab.com>
2025-04-01 00:25:55 +0800

    start achat

--- uachat.pas
+++ uachat.pas
@@ -5,13 +5,31 @@ unit uachat;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+  Classes, SysUtils, DB, Forms, Controls, Graphics, Dialogs, ExtCtrls, DBGrids,
+  ActnList;
 
 type
 
   { Tfrm_achat }
 
   Tfrm_achat = class(TForm)
+    act_edit_cars: TAction;
+    act_edit_order: TAction;
+    act_new_cars: TAction;
+    act_new_order: TAction;
+    ActionList1: TActionList;
+    DBGrid2: TDBGrid;
+    dts_achat_det: TDataSource;
+    dts_achat: TDataSource;
+    DBGrid1: TDBGrid;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    Panel5: TPanel;
+    Panel6: TPanel;
+    Panel7: TPanel;
+    Panel8: TPanel;
   private
 
   public
